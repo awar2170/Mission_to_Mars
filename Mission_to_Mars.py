cc776d33c3b68e8157de01b86a460e96bfc511b4
Alyssa Warnock <awar2170@gmail.com>
2022-02-16 20:51:44 -0500

    Stopped at Customize the Appearance bc its not processing the scraping properly

--- Mission_to_Mars.py
+++ Mission_to_Mars.py
@@ -3,7 +3,21 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
+import datetime as dt
 from webdriver_manager.chrome import ChromeDriverManager
+
+def scrape_all():
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
 
 # Set up Splinter
 executable_path = {'executable_path': ChromeDriverManager().install()}
